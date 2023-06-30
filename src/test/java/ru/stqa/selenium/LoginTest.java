33e4a9a5f8b1624175d333876c24e116b3dc7bc4
Evgeny Kharlamov <xamuron@gmail.com>
2023-06-30 10:08:55 +0300

    updated login test

--- src/test/java/ru/stqa/selenium/LoginTest.java
+++ src/test/java/ru/stqa/selenium/LoginTest.java
@@ -1,2 +1,44 @@
-package ru.stqa.selenium;public class LoginTest {
+package ru.stqa.selenium;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class LoginTest {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    public static final String login = "admin";
+    public static final String password = "admin";
+
+    @BeforeAll
+    public void start() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        driver = new ChromeDriver(options);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    @Test
+    public void loginTest() {
+        driver.get("http://localhost/litecart/admin/");
+        driver.findElement(By.name("username")).sendKeys(login);
+        driver.findElement(By.name("password")).sendKeys(password);
+    }
+
+    @AfterAll
+    public void stop() {
+        driver.quit();
+        driver = null;
+    }
 }
