48022f80710bc33cd3abc9ad0c50d1d9f7fd7680
Evgeny Kharlamov <xamuron@gmail.com>
2023-07-10 00:17:31 +0300

    changed AdminMAinPageTest for ex.6

--- src/test/java/ru/stqa/selenium/AdminMainPageTest.java
+++ src/test/java/ru/stqa/selenium/AdminMainPageTest.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.time.Duration;
 import java.util.List;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -23,7 +22,6 @@ public class AdminMainPageTest {
         options.addArguments("--remote-allow-origins=*");
 
         driver = new ChromeDriver(options);
-        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
 
     public void login(String user, String pass) {
@@ -39,38 +37,20 @@ public class AdminMainPageTest {
 
         List<WebElement> pages = driver.findElements(By.id("app-"));
         pages.get(0).click();
-        int i = 0;
-        int j = 0;
 
-        for (WebElement el: pages) {
-            int finalI = i;
-            // без ожидания не удалось делать клики по элементам, взял пример отсюда https://reflect.run/articles/how-to-deal-with-staleelementreferenceexception-in-selenium/
-            new WebDriverWait(driver, Duration.ofSeconds(10))
-                    .ignoring(StaleElementReferenceException.class)
-                    .until((WebDriver d) -> {
-                        List<WebElement> elements = d.findElements(By.id("app-"));
-                        elements.get(finalI).click();
-                        return true;
-                    });
-            i++;
+        for (int i = 0; i < pages.size() - 1; i++) {
+            List<WebElement> elements = driver.findElements(By.id("app-"));
+            elements.get(i).click();
             Assertions.assertTrue(Utils.isElementPresent(driver, By.cssSelector("h1")));
 
             // если у выбранного пунка меню есть подпункты, то проваливаемся в подпункты
             if (Utils.isElementPresent(driver, By.xpath("*//li[@class='selected'and @id='app-']/ul"))) {
                 List<WebElement> subpages = driver.findElements(By.xpath("*//li[@class='selected'and @id='app-']/ul/li"));
-                for (WebElement subp: subpages) {
-                    int finalJ = j;
-                    new WebDriverWait(driver, Duration.ofSeconds(10))
-                            .ignoring(StaleElementReferenceException.class)
-                            .until((WebDriver d) -> {
-                                List<WebElement> elements = d.findElements(By.xpath("*//li[@class='selected'and @id='app-']/ul/li"));
-                                elements.get(finalJ).click();
-                                return true;
-                            });
-                    j++;
+                for (int j = 0; j < subpages.size() - 1; j ++) {
+                    List<WebElement> subElements = driver.findElements(By.xpath("*//li[@class='selected'and @id='app-']/ul/li"));
+                    subElements.get(j).click();
                     Assertions.assertTrue(Utils.isElementPresent(driver, By.cssSelector("h1")));
                 }
-                j = 0;
             }
         }
     }
